8b46cb633daac629878c8b3d47b0178a8cc12de7
耕萌 曹asxxx <395120787@qq.com>
2019-02-13 16:51:31 +0800

    提交基本代码

---
+++ S03001.SMS/S030011.SMS/Function/SendEmail.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace S030011.SMS.Function
+{
+    public class SendEmail
+    {
+
+        #region 加密 
+
+        /// <summary>
+        /// 加密 
+        /// </summary>
+        public static string Md5WeChat(string rawPass)
+        {
+            // 创建MD5类的默认实例：MD5CryptoServiceProvider  
+            MD5 md5 = MD5.Create();
+            char[] charArray = rawPass.ToCharArray();
+            byte[] bs = new byte[charArray.Length];
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                bs[i] = (byte)charArray[i];
+            }
+
+            byte[] hs = md5.ComputeHash(bs);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hs)
+            {
+                // 以十六进制格式格式化  
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+        public static string Md5Encoding(string rawPass)
+        {
+            rawPass = rawPass + "tamboo";
+            // 创建MD5类的默认实例：MD5CryptoServiceProvider  
+            MD5 md5 = MD5.Create();
+            char[] charArray = rawPass.ToCharArray();
+            byte[] bs = new byte[charArray.Length];
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                bs[i] = (byte)charArray[i];
+            }
+            byte[] hs = md5.ComputeHash(bs);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hs)
+            {
+                // 以十六进制格式格式化  
+                sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+        #region 时间戳
+
+        /// <summary>
+        /// 时间戳
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <returns></returns>
+        public static string GetTimeStamp(int flag)
+        {
+            DateTime ts = DateTime.UtcNow;
+            if (flag == 1)
+            {
+                return ts.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            else
+            {
+                return ts.ToString("yyyy/MM/dd HH:mm:ss");
+            }
+        }
+
+        #endregion
+
+        #region
+
+        /// <summary>
+        /// 发送
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="strContent"></param>
+        public static string PostFunction(string url, string strContent)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            using (StreamWriter dataStream = new StreamWriter(request.GetRequestStream()))
+            {
+                dataStream.Write(strContent);
+                dataStream.Close();
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string encoding = response.ContentEncoding;
+            if (encoding == null || encoding.Length < 1)
+            {
+                encoding = "UTF-8"; //默认编码  
+            }
+
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+            string retString = reader.ReadToEnd();
+            return retString;
+        }
+
+        #endregion
+
+        public static string SendEms(IConfiguration configuration, string receiver, string emailContent)
+        {
+
+            try
+            {
+                string code = "0002";
+                string serviceAddress =configuration["MCserviceAddress"];
+                //总字符串
+                string format = "{{\"uid\":\"{0}\",\"code\":\"{1}\",\"mode\":\"{2}\",\"timestamp\":\"{3}\",\"channel\":\"{4}\",\"data\": [{{\"id\":\"{5}\",\"receiver\":\"{6}\",\"emailContent\":\"{7}\",\"subject\":\"{8}\",\"priority\":\"{9}\",\"origin\":\"{10}\",\"emailId\":\"{11}\",\"deptId\":\"{12}\",\"sendTime\":\"{13}\",\"storageTime\":\"{14}\"}}],\"signCode\":\"{15}\"}}";
+                string uid = "0002";
+                string Mode = null;
+                //string timestamp = "2019/02/12 05:05:58";
+                string timestamp = GetTimeStamp(2);
+                string SendTime = GetTimeStamp(1);
+                // string SendTime = "2019-02-12 05:05:58";
+                string key = "encrypt";
+                string subject = "邮件测试";
+                string priority = "5";
+                string deptId = "0003";
+                //date字符串
+                string Format = "[{{id={0}, receiver={1}, emailContent={2}, subject={3}, priority={4}, origin={5}, emailId={6}, deptId={7}, sendTime={8}, storageTime={9}}}]";
+                string data = string.Format(Format, "null", receiver, emailContent, subject, priority, "null", "null", deptId, SendTime,"null");
+                string signCode = key + data + timestamp + key;
+                string md5SignCode = Md5Encoding(signCode);
+                //邮件内容信息，必须将换行改为\r\n，对面才能接收到\r\n。
+                string emailContenttmp = emailContent.Replace("\r\n", "\\r\\n").Replace("\n", "\\n").Replace("\"", "\\\"");
+                string strContent = string.Format(format, uid, code, Mode, timestamp, "null", "null", receiver, emailContenttmp, subject, priority, "null", "null", deptId, SendTime, "null", md5SignCode);
+                string retString = PostFunction(serviceAddress, strContent);
+                return retString;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+
+        }
+    }
+}
---
+++ S03001.SMS/S030011.SMS/Service/SendEmsService.cs
@@ -0,0 +1,73 @@
+﻿using FD.Simple.Utils.Agent;
+using FD.Simple.Utils.Logging;
+using FD.Simple.Utils.Serialize;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using S030011.SMS.Moudle;
+using System.Linq;
+using S030011.SMS.Interface;
+using S030011.SMS.Function;
+
+namespace S030011.SMS.Service
+{
+    public class SendEmsService : BaseFoo
+    {
+        private IConfiguration _configuration;
+        private IFDLogger _fDLogger;
+        private HttpContext _context;
+
+        public SendEmsService(IFDLogger fDLogger, IConfiguration configuration, IHttpContextAccessor context)
+        {
+            this._fDLogger = fDLogger;
+            this._configuration = configuration;
+            this._context = context.HttpContext;
+        }
+
+        [Routing(EHttpMethod.HttpPost, "SMS/SendEms")]
+        [MessageFormat(EMessageType.Original)]
+        [Anonymous]
+        public string SendEmsasync(string Emailaddress)
+        {
+            var request = _context.Request;
+            System.Console.WriteLine("接收到 Webhook 请求。");
+            var content = string.Empty;
+            try
+            {
+                using (var reader = new StreamReader(request.Body))
+                    content = reader.ReadToEnd();
+                System.Console.WriteLine($"接收到 Webhook 请求：{content}");
+                var con = content.Replace("{", "").Replace("}", "").Replace('\"', '"').Replace('"', '\"');
+                SendEmail.SendEms(_configuration, Emailaddress, con);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine($"处理 Webhook 失败，内容：{content}。");
+                throw;
+            }
+            return "1";
+        }
+
+        [Routing(EHttpMethod.All, "SMS/Err")]
+        [MessageFormat(EMessageType.Original)]
+        [Anonymous]
+        public string SendErr(string Emailaddress)
+        {
+            // string con = "\"id\": \"5c614462cd46e125a4210e6e\",\"url\": \"http://10.50.132.108:50000/#/event/5c614462cd46e125a4210e6e\",\"occurrence_date\": \"2019-02-11T17:46:10.448783+08:00\",\"type\": \"error\",\"message\": \"Value cannot be null.Parameter name: source\",\"project_id\": \"5c3ea1a7cd46e120f8939485\",\"project_name\": \"BaseProject\",\"organization_id\": \"5c3ea1a7cd46e120f8939484\",\"organization_name\": \"NetCore\",\"stack_id\": \"5c614462cd46e125a4210e6d\",\"stack_url\": \"http://10.50.132.108:50000/#/stack/5c614462cd46e125a4210e6d\",\"stack_title\": \"Value cannot be null.Parameter name: source\",\"total_occurrences\": 1,\"first_occurrence\": \"2019-02-11T09:46:10.448783+00:00\",\"last_occurrence\": \"2019-02-11T09:46:10.448783+00:00\",\"is_new\": true,\"is_regression\": false,\"is_critical\": false";
+            // var ret = con.Replace("\"", "");
+            string ret = "\r\n  \"id\": \"5c6230d2cd46e111b4da9ff3\",\r\n  \"url\": \"http://10.50.132.108:50000/#/event/5c6230d2cd46e111b4da9ff3\",\r\n  \"occurrence_date\": \"2019-02-12T10:34:58.2428293+08:00\",\r\n  \"type\": \"error\",\r\n  \"message\": \"Cannot deserialize the current JSON object (e.g. \"name\":\"value\"}) into type 'System.Collections.Generic.List`1[M100011.LoanOrder.Model.CAVEHICLEEXEntity]' because the type requires a JSON array (e.g. [1,2,3]) to deserialize correctly.\r\nTo fix this error either change the JSON to a JSON array (e.g. [1,2,3]) or change the deserialized type so that it is a normal .NET type (e.g. not a primitive type like integer, not a collection type like an array or List<T>) that can be deserialized from a JSON object. JsonObjectAttribute can also be added to the type to force it to deserialize from a JSON object.\r\nPath 'data', line 1, position 8.\",\r\n  \"project_id\": \"5c3ea1a7cd46e120f8939485\",\r\n  \"project_name\": \"BaseProject\",\r\n  \"organization_id\": \"5c3ea1a7cd46e120f8939484\",\r\n  \"organization_name\": \"NetCore\",\r\n  \"stack_id\": \"5c623163cd46e111b4da9ff2\",\r\n  \"stack_url\": \"http://10.50.132.108:50000/#/stack/5c623163cd46e111b4da9ff2\",\r\n  \"stack_title\": \"Cannot deserialize the current JSON object (e.g. \"name\":\"value\"}) into type 'System.Collections.Generic.List`1[M100011.LoanOrder.Model.CAVEHICLEEXEntity]' because the type requires a JSON array (e.g. [1,2,3]) to deserialize correctly.\nTo fix this error either change the JSON to a JSON array (e.g. [1,2,3]) or change the deserialized type so that it is a normal .NET type (e.g. not a primitive type like integer, not a collection type like an array or List<T>) that can be deserialized from a JSON object. JsonObjectAttribute can also be added to the type to force it to deserialize from a JSON object.\nPath 'data', line 1, position 8.\",\r\n  \"total_occurrences\": 1,\r\n  \"first_occurrence\": \"2019-02-12T02:34:58.2428293+00:00\",\r\n  \"last_occurrence\": \"2019-02-12T02:34:58.2428293+00:00\",\r\n  \"is_new\": true,\r\n  \"is_regression\": false,\r\n  \"is_critical\": false\r\n";
+            SendEmail.SendEms(_configuration, Emailaddress, ret);
+            string a = null;
+            //int b = a.Count();
+            var i = new int[2];
+            i[3] = 1;
+            return null;
+        }
+    }
+}
